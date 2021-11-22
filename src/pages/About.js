7943429359e6dfb269aae80723ec84862e4246f4
roopam <roopamgupta68@gmail.com>
2021-11-22 22:10:38 +0530

    first

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>lorem ipsum</p>
+    </section>
+  )
+}
+
+export default About
